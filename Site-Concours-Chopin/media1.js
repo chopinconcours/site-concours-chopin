692b9ae26e5149f0e22c1baf1e3f91ede92cefe4
chopinconcours <chopin.concours@gmail.com>
2025-11-14 16:18:49 +0100

    initial commit

---
+++ Site-Concours-Chopin/media1.js
@@ -0,0 +1,3 @@
+// auto-généré: 2025-08-17 11:51:38
+const mediaGalleries = {"12 mai 2025, finale du concours Chopin à l'EPFL, Lausanne": ["2.mp4", "3.jpg", "4.jpg", "6.jpg", "5.jpg", "1.jpg"], "24 avril 2025, concert à l'Eglise Méthodiste, Lausanne": ["1.jpg", "2.jpg"]};
+const mediaOrder = ["12 mai 2025, finale du concours Chopin à l'EPFL, Lausanne", "24 avril 2025, concert à l'Eglise Méthodiste, Lausanne"];
